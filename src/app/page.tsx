16cb0eed27f9728c34f8b9a1933f29b391e844fc
João Pedro Voltolini <112399200+jpvoltolini@users.noreply.github.com>
2023-05-16 17:14:24 -0300

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/Button";
+
+export default function Home() {
+  return ( 
+    <h1> Hello World
+    <Button/>
+    <Button/>
+    <Button/>
+    <Button/>
+    <Button/>
+    <Button/>
+    <Button/>
+    </h1>
+
+    
+   )
+}
---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+const styles = {
+  color: '#b1cc29',
+}
+
+export function Button(){
+  return (
+    <p style={styles}>teste</p>
+  )
+}
